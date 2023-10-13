853fa21945b9017ea2b0a67c1bc531fb34060aa5
Napomuceno <erjeffersonnapomuceno@gmail.com>
2023-10-13 00:08:51 -0300

    Components CREATED

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,16 @@
+export const dataFake=[
+  {
+    "id":"1",
+    "title":"GOD OF WAR RAGNAROK",
+    "description":"Santa Monica anuncia uma continuação do god of war ragnarol, será que vai se contar a historia do Atreus ou do Kleitinho ?",
+    "photoCover" :"https://cdn.folhape.com.br/upload/dn_arquivo/2022/07/whatsapp-image-2022-07-06-at-161114.jpg"
+
+  },
+  {
+    "id":"2",
+    "title":"No jujutso estamos para descobri quem é o mais forte",
+    "description":" Satoru Gojo realmente é o personagem mais forte do anime jujutsu Kaisen?",
+    "photoCover" :"https://uploads.jovemnerd.com.br/wp-content/uploads/2022/09/jujutsu_kaisen_temporada_2__r0so03b2j-1210x544.jpg"
+
+  },
+]
